604db2b7d44a4d86be878c49f2d9d808421057ed
keeeto <keeeto2000@gmail.com>
2023-10-30 14:45:47 +0000

    changing the model head

---
+++ change-prediction-head/change-head.py
@@ -0,0 +1,104 @@
+import json
+import torch
+
+from pymatgen.core.structure import Structure 
+from pymatgen.io.jarvis import JarvisAtomsAdaptor
+
+from tqdm import tqdm
+
+data = '../trail/PM_90_10.json'  # The data to re-train on
+model_path = '../trail/checkpoint-eform.pt' # The model checkpoint to load initially
+freeze_before = 7 # The layer at which to unfreeze the weights
+
+with open(data, "rb") as f:
+    loaded = json.loads(f.read())
+
+print((loaded[0]))
+dataset = []
+for i in tqdm(range(len(loaded))):
+    info = {}
+    info['jid'] = loaded[i]['jid']
+    info['target'] = loaded[i]['target']
+    info['atoms'] = loaded[i]['atoms']
+    dataset.append(info)
+
+#print(info['atoms'])
+# ### Now set up alignn model
+from alignn.data import get_train_val_loaders
+from jarvis.db.jsonutils import loadjson
+from alignn.config import TrainingConfig
+from alignn.models.alignn import ALIGNN
+
+config = loadjson('../trail/config_git.json')
+config = TrainingConfig(**config)
+
+(
+    train_loader,
+    val_loader,
+    test_loader,
+    prepare_batch,
+) = get_train_val_loaders(
+    dataset_array=dataset,
+    target=config.target,
+    n_train=config.n_train,
+    n_val=config.n_val,
+    n_test=config.n_test,
+    train_ratio=config.train_ratio,
+    val_ratio=config.val_ratio,
+    test_ratio=config.test_ratio,
+    batch_size=config.batch_size,
+    atom_features=config.atom_features,
+    neighbor_strategy=config.neighbor_strategy,
+    standardize=config.atom_features != "cgcnn",
+    id_tag=config.id_tag,
+    pin_memory=config.pin_memory,
+    workers=config.num_workers,
+    save_dataloader=config.save_dataloader,
+    use_canonize=config.use_canonize,
+    filename=config.filename,
+    cutoff=config.cutoff,
+    max_neighbors=config.max_neighbors,
+    output_features=config.model.output_features,
+    classification_threshold=config.classification_threshold,
+    target_multiplication_factor=config.target_multiplication_factor,
+    standard_scalar_and_pca=config.standard_scalar_and_pca,
+    keep_data_order=config.keep_data_order,
+    output_dir=config.output_dir,
+)
+
+
+from alignn.train import train_dgl
+## Check for GPU and CUDA
+device = "cpu"
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+
+model = ALIGNN()
+model.load_state_dict(torch.load(model_path, map_location=torch.device(device))["model"])
+layer_count = 0
+for child in model.children():
+    layer_count += 1
+    if layer_count < freeze_before:
+        for param in child.parameters():
+            param.requires_grad = False
+
+## Redifine the fc module of the model
+model.fc = torch.nn.Sequential(
+    torch.nn.Linear(in_features=256, 
+                    out_features=64, # same number of output units as our number of classes
+                    bias=True),
+    torch.nn.Linear(in_features=64,
+                    out_features=1, # same number of output units as our number of classes
+                    bias=True)).to(device)
+
+model.to(device)
+
+train_dgl(
+    config,
+    train_val_test_loaders=[
+        train_loader,
+        val_loader,
+        test_loader,
+        prepare_batch,
+    ],
+)
